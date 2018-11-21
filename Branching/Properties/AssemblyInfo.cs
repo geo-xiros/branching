82124a581aa09bcb5fe14f997a8ab216e7a78322
george <geo.xiros@gmail.com>
2018-11-21 20:18:10 +0200

    First commit

--- Branching/Properties/AssemblyInfo.cs
+++ Branching/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Branching")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © George 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
